67968e004f752489b9cfaf27665e184f7fd8ba2f
lut0209vn <lut0209vn@gmail.com>
2018-02-09 15:33:52 +0700

    a

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* HELPER: Checks Whether an Element Exists
+----------------------------------------------------------------------------------------------------*/
+(function( $ ) {
+
+  $.fn.extend({
+    exists: function() {
+      if ( this.length > 0 ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+
+})( jQuery );
+
